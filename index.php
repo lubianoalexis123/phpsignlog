2c5ebdce59dcdbc1b1edd3ea490cba61597b084f
lubianoalexis123 <31789109+lubianoalexis123@users.noreply.github.com>
2017-09-30 09:13:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,36 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Untitled Document</title>
+<title>login_signup</title>
 <link href="boilerplate.css" rel="stylesheet" type="text/css">
 <link href="../../../Users/PC-User/Desktop/it11/Unnamed Site 3/signup.php" rel="stylesheet" type="text/css">
+<style type="text/css">
+#sign_in {
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 25px;
+}
+#login {
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 25px;
+}
+#user {
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 15px;
+}
+#uname {
+	font-family: "Arial Black", Gadget, sans-serif;
+	font-size: 15px;
+}
+#bgi {
+	width: 100%;
+	position: fixed;
+	z-index: -100;
+	left: 0px;
+	top: 0px;
+	min-height: 100%;
+	min-width: 100%;
+}
+</style>
 <!-- 
 To learn more about the conditional comments around the html tags at the top of the file:
 paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
@@ -29,23 +56,51 @@ Do the following if you're using your customized build of modernizr (http://www.
 <title>Basic Login using PHP and MySql</title>
 </head>
 <body>
-	<form method="post" action="login.php"> 
+<form method="post" action="login.php"> 
 	  <p>        
+	  <p><img src="background.jpg" alt="" name="bgi" width="1600" height="900" id="bgi">        
 	  <p>        
-	  <p>        
-	  <p id="login">LOGIN	
-	  <p>        Username: 
-	    <input type="text" name="txtuname" />
+  <blockquote>
+	    <blockquote>
+	      <blockquote>
+	        <blockquote>
+	          <p align="left" id="login">LOGIN	
+              </p>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+  </blockquote>
+  <p align="center" id="user">        Username: 
+    <input type="text" name="txtuname" />
 Password:		  
   <input type="password" name="txtpword">
-		<input type="submit" name="login" value="login">
-      <p id="sign_in">SIGN IN
+	<input type="submit" name="login" value="login">
+     
 </form>
-    	
-    <form method="post" action="signup.php"> 
-	<p>Username:     <input type="text" name="txtuname" />
-      Password:
-		<input type="password" name="txtpword">
-		<input type="submit" name="signup" value="Signup">
+    	 <blockquote>
+    	   <blockquote>
+    	     <blockquote>
+    	       <blockquote>
+    	         <p id="sign_in">&nbsp;</p>
+  	         </blockquote>
+  	         </blockquote>
+  	       </blockquote>
+       </blockquote>
+<form method="post" action="signup.php">  
+  <blockquote>
+    <blockquote>
+      <blockquote>
+        <blockquote>
+          <p id="sign_in"> SIGN IN
+        </p>
+        </blockquote>
+      </blockquote>
+    </blockquote>
+  </blockquote>
+  <p align="center" id="uname">    Username:     
+	<input type="text" name="txtuname" />
+    Password:
+	  <input type="password" name="txtpword">
+	  <input type="submit" name="signup" value="Signup">
 </form> 
 </body>
